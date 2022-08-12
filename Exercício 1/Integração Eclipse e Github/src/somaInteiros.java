6a98e9c79e9d87c4c077d6f85848cbe09e6a9c84
Rafael Fleury <rfbcol@gmail.com>
2022-08-12 15:13:11 -0300

    feito soma de inteiros
    programa que calcula a soma de dois números inteiros digitados no terminal

--- Exercício 1/Integração Eclipse e Github/src/somaInteiros.java
+++ Exercício 1/Integração Eclipse e Github/src/somaInteiros.java
@@ -1,7 +1,21 @@
+import java.util.Scanner;
+
 public class somaInteiros {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello");
-		System.out.println("feito 2");
+		System.out.println("Digite um numero inteiro: ");
+		Scanner leitor = new Scanner(System.in);
+		int num1 = leitor.nextInt();
+		
+		leitor.close();
+		System.out.println("Digite outro numero inteiro: ");
+		leitor = new Scanner(System.in);
+		int num2 = leitor.nextInt();
+		leitor.close();
+		
+		int resposta = num1 + num2;
+		
+		System.out.println("A soma dos dois numeros eh: " + resposta);
+		
 	}
 }
